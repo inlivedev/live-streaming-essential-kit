25e25a24abceea8f0f101f2c71c9f9ded7c681fe
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2022-10-10 14:18:34 +0700

    feat: add esint config files

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:jsdoc/recommended',
+    'plugin:promise/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'jsdoc',
+    'prettier',
+    'promise',
+  ],
+  ignorePatterns: ['node_modules']
+}
