2bea001d75be487c05624169e9ef5412a809b508
diah2 <diahlestia96@gmail.com>
2022-10-14 18:11:34 +0700

    prettier formatting

--- src/pages/api/auth/[name].js
+++ src/pages/api/auth/[name].js
@@ -1,78 +1,61 @@
 import jwt from 'jsonwebtoken';
 
 const handler = async (request, reply) => {
+  const name = request?.params?.name;
 
-  const name = request?.params?.name
-
-  const { JWT_SECRET, JWT_EXPIRES_IN } = process.env
+  const { JWT_SECRET, JWT_EXPIRES_IN } = process.env;
 
   if (name == 'login') {
+    try {
+      const { username, password } = request.body;
+
+      const valid_credentials =
+        username === 'username' && password === 'password';
+
+      if (!valid_credentials)
+        reply.code(400).send({ message: 'Invalid credentials' });
+
+      reply.code(200).send({ success: true });
+    } catch (err) {
+      throw new Error(err);
+    }
+  } else if (name == 'generateToken') {
+    const { username, password } = request.body;
+
+    const valid_credentials =
+      username === 'username' && password === 'password';
+
+    if (!valid_credentials)
+      reply.code(400).send({ message: 'Invalid credentials' });
 
     try {
-        const { username, password } = request.body
+      const token = jwt.sign(
+        { username: `${username}${Date.now()}` },
+        JWT_SECRET,
+        {
+          expiresIn: JWT_EXPIRES_IN
+        }
+      );
 
-        const valid_credentials = username === 'username' && password === 'password';
-
-        if(!valid_credentials)
-            reply
-            .code(400)
-            .send({ message: 'Invalid credentials' });
-        
-        reply
-            .code(200)
-            .send({ success: true });
+      reply.code(200).send({ token });
+    } catch (err) {
+      throw new Error(err);
     }
-    catch (err) {
-        throw new Error(err)
-    }
-    
-  }
-  
-  else if (name == 'generateToken') {
-
-    const { username, password } = request.body
-
-    const valid_credentials = username === 'username' && password === 'password';
-
-    if(!valid_credentials)
-        reply
-            .code(400)
-            .send({ message: 'Invalid credentials' });
-
+  } else if (name === 'validation') {
     try {
-        const token = jwt.sign({ username: `${username}${Date.now()}` }, JWT_SECRET, {
-            expiresIn: JWT_EXPIRES_IN
-          });
-      
-          reply
-              .code(200)
-              .send({ token });
-    }
-    catch(err) {
-        throw new Error(err)
-    }
-  } 
-  else if (name === 'validation') {
-    try {
-        if (!request.headers.authorization) 
-            reply
-                .code(401)
-                .send({message: 'You are not authorize'})
-        
-        const token = request.headers.authorization.replace('Bearer ', '')
+      if (!request.headers.authorization)
+        reply.code(401).send({ message: 'You are not authorize' });
 
-        const verify = await jwt.verify(token, JWT_SECRET);
+      const token = request.headers.authorization.replace('Bearer ', '');
 
-        if (parseInt(Date.now()) < parseInt(verify.exp)) 
-            reply
-                .code(200)
-                .send({ success: true });
+      const verify = await jwt.verify(token, JWT_SECRET);
+
+      if (Date.now() < parseInt(verify.exp))
+        reply.code(200).send({ success: true });
+    } catch (err) {
+      throw new Error(err);
     }
-    catch(err){
-        throw new Error(err);
-    }
-    
   }
 };
-  
-  export default handler;  
+
+export default handler;
