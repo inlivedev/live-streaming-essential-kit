86f375b1a9a731fd9e06c1692d20199cb80deb98
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2022-10-23 15:10:08 +0700

    feat: create a new embed page
    - this embed page consists only of player in fullscreen mode.
    - this page purpose is to be included in the iframe

---
+++ src/pages/streaming/embed/[streamid].js
@@ -0,0 +1,40 @@
+import { html } from 'lit';
+import { InliveStream } from '@inlivedev/inlive-js-sdk/stream';
+
+const EmbedStreamingPage = (properties) => {
+  const { hlsManifest, dashManifest } = properties;
+
+  return html`
+    <app-player
+      class="full-screen"
+      src=${hlsManifest}
+      dashUrl=${dashManifest}
+      hlsUrl=${hlsManifest}
+      autoplay
+      muted
+    ></app-player>
+  `;
+};
+
+export const scripts = `
+  <script type="module" src="/__client/features/player/app-player.js"></script>
+`;
+
+export default EmbedStreamingPage;
+
+export const getServerSideProps = async (request) => {
+  const streamId = Number.parseInt(request.params.streamid, 10);
+  const streamResponse = await InliveStream.getStream(streamId);
+  let streamData = {};
+
+  if (streamResponse.status.code === 200 && streamResponse.data) {
+    streamData = streamResponse.data;
+  }
+
+  return {
+    props: {
+      hlsManifest: streamData.hlsManifestPath || '',
+      dashManifest: streamData.dashManifestPath || ''
+    }
+  };
+};
