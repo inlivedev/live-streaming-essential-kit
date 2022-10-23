1a9c94d3dabbfdf185614194f02619d2115eb517
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2022-10-23 16:05:15 +0700

    feat: create a stream capturer component
    - this stream capturer will capture the media device that will be used by the client and initialize the  peer connection between the local peer and remote peer.
    
    - it will also prepare and initialize a stream session

---
+++ src/features/studio/app-stream-capturer.js
@@ -0,0 +1,107 @@
+import { LitElement } from 'lit';
+import { InliveStream } from '@inlivedev/inlive-js-sdk/stream';
+import { fetchHttp } from '../shared/modules/fetch-http.js';
+
+export class AppStreamCapturer extends LitElement {
+  static properties = {
+    endTime: { type: String },
+    startTime: { type: String },
+    streamId: { type: Number },
+    preparedAt: { type: String },
+    videoElement: { type: Object }
+  };
+
+  constructor() {
+    super();
+    /** @type {string | undefined} */
+    this.endTime = undefined;
+    /** @type {string | undefined} */
+    this.startTime = undefined;
+    /** @type {number | undefined} */
+    this.streamId = undefined;
+    /** @type {string | undefined} */
+    this.preparedAt = undefined;
+    /** @type {object | undefined} */
+    this.videoElement = undefined;
+  }
+
+  /**
+   *
+   * @param {import('lit').PropertyValues<any>} changedProperties - The component properties that change
+   */
+  firstUpdated(changedProperties) {
+    if (changedProperties.has('videoElement') && !this.endTime) {
+      this.initializeCapturer(this.videoElement);
+    }
+  }
+
+  /**
+   *
+   * @param {object | undefined} videoElement - The video element
+   */
+  async initializeCapturer(videoElement) {
+    const defaultMediaConstraints = {
+      video: {
+        frameRate: 30,
+        width: { min: 640, ideal: 1280, max: 1280 },
+        height: { min: 360, ideal: 720, max: 720 }
+      },
+      audio: true
+    };
+
+    /** @type {MediaStream} */
+    const mediaStream = await InliveStream.media.getUserMedia(
+      defaultMediaConstraints
+    );
+
+    const mediaElement = InliveStream.media.attachMediaElement(
+      videoElement,
+      mediaStream
+    );
+
+    const connection = await InliveStream.connection.open({
+      streamId: this.streamId,
+      mediaStream,
+      mediaElement
+    });
+
+    const peerConnection = connection.getPeerConnection();
+
+    if (!this.preparedAt) {
+      try {
+        await fetchHttp({
+          url: `/api/stream/prepare`,
+          method: 'POST',
+          body: {
+            streamId: this.streamId
+          }
+        });
+      } catch (error) {
+        console.error('Error on stream preparation', error);
+      }
+    } else {
+      try {
+        const streamInitialization = await fetchHttp({
+          url: `/api/stream/init`,
+          method: 'POST',
+          body: {
+            streamId: this.streamId,
+            sessionDescription: peerConnection.localDescription
+          }
+        });
+
+        if (
+          streamInitialization.status.code &&
+          typeof streamInitialization.data === 'object'
+        ) {
+          const remoteSessionDescription = streamInitialization.data;
+          connection.connect(remoteSessionDescription);
+        }
+      } catch (error) {
+        console.error('Error on stream initialization', error);
+      }
+    }
+  }
+}
+
+customElements.define('app-stream-capturer', AppStreamCapturer);
