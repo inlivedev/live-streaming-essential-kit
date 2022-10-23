50b467aab716da4a7e3590e7dc36c3682aa37370
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2022-10-23 13:54:44 +0700

    feat: integrate video with the stream capturer

--- src/features/studio/app-video-panel.js
+++ src/features/studio/app-video-panel.js
@@ -1,6 +1,11 @@
 import { html, LitElement, css } from 'lit';
+import './app-stream-capturer.js';
 
-class AppVideoPanel extends LitElement {
+/**
+ * @typedef {import('./app-stream-capturer.js').AppStreamCapturer} AppStreamCapturer
+ */
+
+export class AppVideoPanel extends LitElement {
   static styles = css`
     :host {
       display: flex;
@@ -11,7 +16,7 @@ class AppVideoPanel extends LitElement {
 
     .container {
       flex: 1;
-      background-color: #555;
+      background-color: #121212;
     }
 
     @media (min-width: 1024px) {
@@ -21,6 +26,23 @@ class AppVideoPanel extends LitElement {
       }
     }
 
+    .video-placeholder {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      color: white;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      text-align: center;
+      font-size: 1rem;
+      line-height: 1.5rem;
+      padding: 1rem;
+      box-sizing: border-box;
+    }
+
     .video-ratio {
       position: relative;
       width: 100%;
@@ -28,7 +50,7 @@ class AppVideoPanel extends LitElement {
       padding-top: calc(9 / 16 * 100%); /* 16:9 ratio */
     }
 
-    .video-player {
+    .video-element {
       width: 100%;
       height: 100%;
       position: absolute;
@@ -38,17 +60,65 @@ class AppVideoPanel extends LitElement {
     }
   `;
 
+  static properties = {
+    endTime: { type: String },
+    startTime: { type: String },
+    streamId: { type: Number },
+    preparedAt: { type: String }
+  };
+
+  constructor() {
+    super();
+    /** @type {string | undefined} */
+    this.endTime = undefined;
+    /** @type {string | undefined} */
+    this.startTime = undefined;
+    /** @type {number | undefined} */
+    this.streamId = undefined;
+    /** @type {string | undefined} */
+    this.preparedAt = undefined;
+  }
+
+  firstUpdated() {
+    this.attachVideoElement();
+  }
+
+  attachVideoElement() {
+    const videoElement = this.videoElement;
+    const streamCapturer = this.streamCapturer;
+
+    if (videoElement && streamCapturer) {
+      streamCapturer.videoElement = videoElement;
+    }
+  }
+
+  get streamCapturer() {
+    return /** @type {AppStreamCapturer} */ (
+      this.renderRoot.querySelector('app-stream-capturer')
+    );
+  }
+
+  get videoElement() {
+    return /** @type {HTMLVideoElement} */ (
+      this.renderRoot.querySelector('.video-element')
+    );
+  }
+
   render() {
     return html`
+      <app-stream-capturer
+        streamId=${this.streamId}
+        startTime=${this.startTime}
+        endTime=${this.endTime}
+        preparedAt=${this.preparedAt}
+      ></app-stream-capturer>
       <div class="container">
         <div class="video-ratio">
-          <video
-            id="video-player"
-            class="video-player"
-            autoplay
-            muted
-            playsinline
-          ></video>
+          ${this.startTime && this.endTime
+            ? html`
+                <div class="video-placeholder">The live stream has ended.</div>
+              `
+            : html` <video class="video-element"></video> `}
         </div>
       </div>
     `;
