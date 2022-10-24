5b0f0ad040e41889d28f605369591831a2d5ce99
diahasumsi <114555776+diahasumsi@users.noreply.github.com>
2022-10-24 08:36:57 +0700

    Feat : Create api proxy handler for create until end stream (#19)
    * create handler for create until end stream
    
    * add env example
    
    * Revert "Merge branch 'feat/api-proxy' of https://github.com/inlivedev/live-streaming-essential-kit into feat/api-proxy"
    
    This reverts commit 9d44b795333b979936d75d57f36a37555d65c314, reversing
    changes made to 3a4d5f37a12db3f12df2badd9081245a56930d27.
    
    * move validation to regular function
    
    * move validation to regular function, remove validation inside auth api
    
    * fixing error trial code
    
    * adding handler for init and change handler for prepare, fix error unicorn to swith-case instead of if-else
    
    * handle request method validation
    
    * move validation function to feature directory
    
    * change response
    
    * fix: follow eslint rule
    
    * fix: Failed to load plugin 'unicorn' package.json
    
    * fix: remove unnecessary try catch
    
    - remove the try catch because fastify automatically tries to catch as many uncaught errors
    
    * refactor: remove unnecessary comments
    
    * refactor: handle server error when there is no input
    
    - let the sdk handle and throw the error when the input is not valid
    
    * refactor: remove wrong jsdoc comment
    
    * fix: remove duplicate dash
    
    * refactor: change comment
    
    * refactor: code improvement
    
    Co-authored-by: Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>

---
+++ src/pages/api/stream/[name].js
@@ -0,0 +1,88 @@
+import { InliveApp } from '@inlivedev/inlive-js-sdk/app';
+import { InliveStream } from '@inlivedev/inlive-js-sdk/stream';
+import { validation } from '../../../features/auth/validation.js';
+
+const handler = async (request, reply) => {
+  const name = request.params?.name;
+
+  if (request.method.toUpperCase() !== 'POST') {
+    reply.code(400).send({
+      code: 400,
+      message: 'Method is not allowed'
+    });
+  }
+  /**
+   * call a function to validate user
+   */
+  let validUser = await validation(request);
+
+  if (!validUser)
+    reply.code(403).send({
+      code: 403,
+      message: 'You are not authorized'
+    });
+
+  /**
+   * @typedef Config
+   * @property {string} api_key - A string of key that will be used to access inLive protected API
+   */
+
+  /**
+   * A variable to create object
+   *
+   * @param {Config} config - The initialization configuration
+   */
+  let config = {
+    // eslint-disable-next-line camelcase
+    api_key: process.env.API_KEY
+  };
+
+  const inliveApp = InliveApp.init(config);
+
+  switch (name) {
+    case 'create': {
+      let dataStream;
+
+      const configObject = {
+        name: request.body?.name || '',
+        description: request.body?.description || ''
+      };
+
+      dataStream = await InliveStream.createStream(inliveApp, configObject);
+      reply.code(dataStream.status.code).send(dataStream);
+      break;
+    }
+    case 'prepare':
+    case 'start':
+    case 'end': {
+      let dataStream;
+
+      const functionName = name + 'Stream';
+
+      const configObject = {
+        streamId: request.body.streamId
+      };
+
+      dataStream = await InliveStream[functionName](inliveApp, configObject);
+      reply.code(dataStream.status.code).send(dataStream);
+      break;
+    }
+    case 'init': {
+      let dataStream;
+
+      const configObject = {
+        streamId: request.body.streamId,
+        sessionDescription: request.body.sessionDescription
+      };
+
+      dataStream = await InliveStream.initStream(inliveApp, configObject);
+      reply.code(dataStream.status.code).send(dataStream);
+      break;
+    }
+    default: {
+      break;
+    }
+  }
+};
+
+export default handler;
