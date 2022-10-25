c38a786d4671aa5b6fc9e14df9658b3f534805e7
Ingrid <ingrid.fortunata@asumsi.co>
2022-10-25 17:41:35 +0700

    rename handle timer var

--- src/features/shared/modules/handle-timer.js
+++ src/features/shared/modules/handle-timer.js
@@ -12,8 +12,8 @@ export const handleTimer = (
   if (streamStatus === 'live' && appTimerCounter) {
     //when the stream is live but the user just reload the page, the timer will get the time when the stream started
     if (startTime) {
-      const timeStartTime = new Date(startTime).getTime();
-      appTimerCounter.handleStartTimer(timeStartTime);
+      const startTimeInNumber = new Date(startTime).getTime();
+      appTimerCounter.handleStartTimer(startTimeInNumber);
     } else {
       //we don't need start time param when the stream is just started to live
       appTimerCounter.handleStartTimer();
@@ -23,17 +23,17 @@ export const handleTimer = (
 
     //when loaded on first time, if the stream has already ended, will get the previous stream duration
     if (startTime && endTime) {
-      const timeStartTime = new Date(startTime).getTime();
-      const timeEndTime = new Date(endTime).getTime();
-      const substractTime = timeEndTime - timeStartTime;
+      const startTimeInNumber = new Date(startTime).getTime();
+      const endTimeInNumber = new Date(endTime).getTime();
+      const substractTime = endTimeInNumber - startTimeInNumber;
       appTimerCounter.handleFormatTime(substractTime);
     }
 
     //when loaded on first time, if the stream hasn't ended yet (still on going), will get the stream duration up until now
     if (startTime && !endTime) {
-      const timeStartTime = new Date(startTime).getTime();
+      const startTimeInNumber = new Date(startTime).getTime();
       const timeNow = Date.now();
-      const substractTime = timeNow - timeStartTime;
+      const substractTime = timeNow - startTimeInNumber;
       appTimerCounter.handleFormatTime(substractTime);
     }
   }
