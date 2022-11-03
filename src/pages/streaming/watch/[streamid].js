0cb1fe66afc513e7cc5a081ac7b5dd6bb896adbb
Ingrid <102952824+ingrid011@users.noreply.github.com>
2022-11-03 16:10:49 +0700

    refactor inlive init & add init object in getStream (#32)

--- src/pages/streaming/watch/[streamid].js
+++ src/pages/streaming/watch/[streamid].js
@@ -1,5 +1,6 @@
 import { html } from 'lit';
 import { InliveStream } from '@inlivedev/inlive-js-sdk/stream';
+import { initialization } from '../../../features/shared/modules/initialization.js';
 
 const WatchStreamingPage = (properties) => {
   const { heading, description, hlsManifest, dashManifest } = properties;
@@ -22,8 +23,10 @@ export const scripts = `
 export default WatchStreamingPage;
 
 export const getServerSideProps = async (request) => {
+  const inliveApp = initialization();
+
   const streamId = Number.parseInt(request.params.streamid, 10);
-  const streamResponse = await InliveStream.getStream(streamId);
+  const streamResponse = await InliveStream.getStream(inliveApp, streamId);
   let streamData = {};
 
   if (streamResponse.status.code === 200 && streamResponse.data) {
